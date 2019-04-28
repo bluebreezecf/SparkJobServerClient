b37399794d58c88f9b5916f645e3200434e5f208
knvs97 <knvs97@gmail.com>
2019-04-28 17:54:55 +0530

    Set Default ConnectionTimeOut, SocketTimeout fix

--- src/main/java/com/bluebreezecf/tools/sparkjobserver/api/SparkJobServerClientImpl.java
+++ src/main/java/com/bluebreezecf/tools/sparkjobserver/api/SparkJobServerClientImpl.java
@@ -103,9 +103,9 @@ class SparkJobServerClientImpl implements ISparkJobServerClient {
 			jobServerUrl = jobServerUrl + "/";
 		}
 		this.jobServerUrl = jobServerUrl;
-		this.socketTimeOut = connectionTimeOut == null? DEFAULT_SOCKET_TIMEOUT : connectionTimeOut;
+		this.socketTimeOut = connectionTimeOut == null? DEFAULT_CONNECTION_TIMEOUT : connectionTimeOut;
 		this.connectionReqTimeOut = connectionReqTimeOut == null ? DEFAULT_REQUEST_TIMEOUT : connectionReqTimeOut;
-		this.connectionTimeOut = socketTimeOut == null ? DEFAULT_CONNECTION_TIMEOUT : socketTimeOut;
+		this.connectionTimeOut = socketTimeOut == null ? DEFAULT_SOCKET_TIMEOUT : socketTimeOut;
 	}
 
     /**
